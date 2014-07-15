e41f8628157ed8284cf322ef2fa81963caaa8a39
Nahiyan Alamgir <alnahiyan97@gmail.com>
2014-07-15 13:31:35 +0600

    Fixed stuff

--- app/config/database.php
+++ app/config/database.php
@@ -49,7 +49,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'model-demo',
+			'database'  => 'model_demo',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
